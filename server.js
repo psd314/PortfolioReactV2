94599a48cc06edfebf54d3c85648b1e64ad591ce
Philippe Dixon <pdixon555@hotmail.com>
2018-01-21 15:23:08 -0500

    added server and deploying to heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
+
+app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
