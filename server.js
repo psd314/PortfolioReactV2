7b3666b1a33c936e7a8a4e5eba9a0fdddf67520c
Philippe Dixon <pdixon555@hotmail.com>
2018-01-23 19:16:12 -0500

    remove commented code

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.use(express.static("client/build"));
 app.use(express.static("client/build"));
 
 app.use(function(req, res) {
